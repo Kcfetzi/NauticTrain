87868d75c03c6c7983da55a6a9b01fa58793892c
Martin Bla <zedler89@googlemail.com>
2024-10-14 13:03:05 +0200

    bit of ui

--- Assets/Nautic/UI_Root/Systems/Mobile_UI/Scripts/Cockpit/CockpitUIController.cs
+++ Assets/Nautic/UI_Root/Systems/Mobile_UI/Scripts/Cockpit/CockpitUIController.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class CockpitUIController : MonoBehaviour
 {
     [Header("RuderCanvas")]
-    [SerializeField] private RectTransform _ruderImage;
+    [SerializeField] private RectTransform _ruderDisplayImage;
 
     [Header("ShipValuesCanvas")]
     [SerializeField] private TMP_Text _velocityOverGroundText;
@@ -17,63 +17,55 @@ public class CockpitUIController : MonoBehaviour
     [SerializeField] private RectTransform _rect;
 
     [SerializeField] private RuderKnob _ruderKnob;
-    [SerializeField] private Slider _thrustSlider;
-    
-    [SerializeField] private TMP_Text _ruderText;
-    [SerializeField] private TMP_Text _thrustText;
-    
-    [SerializeField] private TMP_Text _wantedThrustText;
-    [SerializeField] private TMP_Text _wantedCourseText;
-    
-    [SerializeField] private GameObject _thrustChanger;
+    [SerializeField] private CourseChanger _courseChanger;
+    [SerializeField] private ThrustSlider _thrustSlider;
     
     private ObjectContainer _displayedData;
     
     // user input datea
     private float _lastTimeUserTouched = 0;
     private float _wantedTrust;
+    private float _wantedRuder;
     private float _wantedCourse;
 
     // bool to determine if course is hardset or ruder is used
     private bool _setCourse;
     
-    private void Awake()
-    {
-        _thrustSlider.onValueChanged.AddListener(AddToWantedThrust);
-    }
-
     private void Update()
     {
         if (!_displayedData)
             return;
 
+        // show velocity and course in top right display
         _velocityOverGroundText.text = "VOG: " + _displayedData.ActualVelocity;
         _courseOverGroundText.text = "COG: " + _displayedData.ActualCourse;
+        // always show actual course in coursechanger
+        _courseChanger.SetCourse(_displayedData.ActualCourse);
         
         if (!_displayedData.UserInteract)
         {
-            _ruderImage.localRotation = Quaternion.Euler(new Vector3(0, 0, _displayedData.RuderValue));
+            // left top panel with ruder display
+            _ruderDisplayImage.localRotation = Quaternion.Euler(new Vector3(0, 0, _displayedData.RuderValue));
             
             _ruderKnob.SetRotation(_displayedData.RuderValue);
-            _thrustSlider.SetValueWithoutNotify(_displayedData.ThrustValue);
-            
-            _ruderText.text = _displayedData.RuderValue + " \u00B0";
-            _wantedThrustText.text = _displayedData.ThrustValue.ToString();
-            _thrustText.text = _displayedData.ThrustValue.ToString();
-            _wantedTrust = _displayedData.ThrustValue;
+            _thrustSlider.SetThrust(_displayedData.ThrustValue);
         } else 
         {
+            // does user interact with cockpit in last 3 seconds?
             if (Time.time - _lastTimeUserTouched > 3)
             {
-                _displayedData.ThrustValue = Int32.Parse(_wantedThrustText.text.Substring(0, _wantedThrustText.text.Length -3));
+                _displayedData.ThrustValue = _wantedTrust;
+                // check if user want to set a course or set the ruder
                 if (_setCourse)
                 {
                     _displayedData.WantedCourse = _wantedCourse;
                 } else
                 {
-                    _displayedData.RuderValue = _ruderKnob.Rotation;
+                    _displayedData.RuderValue = _wantedRuder;
                 }
                 _setCourse = false;
+                
+                // delete user interacted flag
                 _displayedData.UserInteract = false;
                 _displayedData.OnUserInteractionStopped?.Invoke();
             }
@@ -82,78 +74,49 @@ public class CockpitUIController : MonoBehaviour
 
     public void SetSelectedNauticObject(NauticObject obj)
     {
-        if (!obj)
-        {
-            _thrustSlider.enabled = false;
-            _thrustChanger.SetActive(false);
-            return;
-        }
-
-        _thrustSlider.enabled = true;
-        _thrustChanger.SetActive(true);
-        
         _displayedData = obj.Data;
-        
-        _thrustSlider.value = _displayedData.ThrustValue;
-        _thrustText.text = _displayedData.ThrustValue + " KN";
-        
-        _ruderKnob.SetRotation(_displayedData.RuderValue);
-        _ruderImage.localRotation = Quaternion.Euler(new Vector3(0, 0, _displayedData.RuderValue));
-        _ruderText.text = _displayedData.RuderValue + " \u00B0";
     }
     
-    private void AddToWantedThrust(float amount)
+    public void SetWantedThrust(int amount)
     {
+        // mark user interaction
          if (!_displayedData.UserInteract)
          {
              _displayedData.UserInteract = true;
              _displayedData.OnUserInteractionStarted?.Invoke();
          }
          
+        
+         _wantedTrust = amount;
          _lastTimeUserTouched = Time.time;
-         _wantedTrust = Mathf.Clamp(amount,-5, 18);
-         _wantedThrustText.text = _wantedTrust + " KN";
     }
 
-    public void SetWantedRuder(float amount)
+    // user set ruder on ruderknob, set user interacted flag
+    public void SetWantedRuder(int amount)
     {
+        // mark user interaction
         if (!_displayedData.UserInteract)
         {
             _displayedData.UserInteract = true;
             _displayedData.OnUserInteractionStarted?.Invoke();
         }
 
-        _ruderText.text = amount.ToString("F0") +  " \u00B0";
+        _wantedRuder = amount;
         _lastTimeUserTouched = Time.time;
     }
     
-    public void AddToWantedDegree(int amount)
+    public void SetWantedCourse(int amount)
     {
+        // mark user interaction
         if (!_displayedData.UserInteract)
         {
             _displayedData.UserInteract = true;
             _displayedData.OnUserInteractionStarted?.Invoke();
         }
-        _setCourse = true;
-        _lastTimeUserTouched = Time.time;
 
-        _wantedCourse += amount;
-        if (_wantedCourse < 0)
-            _wantedCourse += 360;
-        
-        if (Math.Abs(_wantedCourse) >= 360)
-            _wantedCourse %= 360;
-        
-        if (_wantedCourse < 100)
-            if (_wantedCourse < 10)
-                _wantedCourseText.text = "00" + _wantedCourse;
-            else
-            {
-                _wantedCourseText.text = "0" + _wantedCourse;
-            }
-        else 
-            _wantedCourseText.text = _wantedCourse.ToString();
-        
+        _setCourse = true;
+        _wantedCourse = amount;
+        _lastTimeUserTouched = Time.time;
     }
 
     public void ToggleControllPanel()
@@ -168,5 +131,11 @@ public class CockpitUIController : MonoBehaviour
             _panel.anchoredPosition = new Vector2(0, 175);
             _rect.localRotation = Quaternion.Euler(new Vector3(0, 0, 180));
         }
+    }
+    
+    public void ToggleRuder(bool active)
+    {
+        _ruderKnob.gameObject.SetActive(active);
+        _courseChanger.gameObject.SetActive(!active);
     }
 }
--- Assets/Nautic/UI_Root/Systems/Mobile_UI/Scripts/Cockpit/RuderKnob.cs
+++ Assets/Nautic/UI_Root/Systems/Mobile_UI/Scripts/Cockpit/RuderKnob.cs
@@ -14,23 +14,26 @@ public class RuderKnob : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
     [SerializeField] private float _rotationSpeed;
     
     private Vector3 _rotationVector = Vector3.zero;
-    
+
+    private float _mouseStartX;
     private bool _isDragging;
 
-    public float Rotation => Mathf.Round((45f / 105f) * _rotationVector.z);
+    // use this to get the real value, because the image rotation values are not real
+    public float Rotation => Mathf.Round((50f / 105f) * _rotationVector.z);
     
     void Update()
     {
         if (_isDragging)
         {
-            float mouseX = Input.GetAxis("Mouse X");
-            _rotationVector.z -= mouseX * _rotationSpeed;
+            float diffX = Input.GetAxis("Mouse X") - _mouseStartX;
+            _rotationVector.z -= diffX * _rotationSpeed;
             _rotationVector.z = Mathf.Round(_rotationVector.z);
             _rotationVector.z = Mathf.Clamp(_rotationVector.z, -105, 105);
 
             _ruderHandleImage.rotation = Quaternion.Euler(_rotationVector);
-            _cockpitUIController.SetWantedRuder(_rotationVector.z);
             _rotationText.text = Mathf.Abs(Rotation) + "\u00B0";
+            
+            _cockpitUIController.SetWantedRuder((int)Rotation);
         }
     }
 
@@ -43,6 +46,7 @@ public class RuderKnob : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
     public void OnPointerDown(PointerEventData eventData)
     {
         _isDragging = true;
+        _mouseStartX = Input.GetAxis("Mouse X");
     }
 
     public void SetRotation(float rotation)
